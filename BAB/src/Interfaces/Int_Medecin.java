b392a5b96744378b58dde9a8fd3d1594dd5fef2d
mohamed <mohamed@10.214.20.69>
2019-03-09 21:32:21 +0000

    Lah yamolana lah lah yalwahd

--- BAB/src/Interfaces/Int_Medecin.java
+++ BAB/src/Interfaces/Int_Medecin.java
@@ -100,35 +100,78 @@ public class Int_Medecin extends JFrame {
 		lblImandknsjksd.setBounds(0, 13, 187, 44);
 		panel_3.add(lblImandknsjksd);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(0, 90, 866, 463);
-		panel_1.add(panel_2);
-		panel_2.setLayout(null);
+		JPanel AcceuilPanel = new JPanel();
+		AcceuilPanel.setBounds(0, 90, 866, 463);
+		panel_1.add(AcceuilPanel);
+		AcceuilPanel.setLayout(null);
+		
+		JLabel lblNewLabel_4 = new JLabel("     Rendez vous");
+		lblNewLabel_4.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
+		lblNewLabel_4.setIcon(new ImageIcon("ressources\\icons8-health-checkup-30.png"));
+		lblNewLabel_4.setBounds(22, 274, 185, 39);
+		AcceuilPanel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_3 = new JLabel("    Nombre Patients");
+		lblNewLabel_3.setIcon(new ImageIcon("ressources\\icons8-appointment-scheduling-30.png"));
+		lblNewLabel_3.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
+		lblNewLabel_3.setBounds(22, 219, 185, 39);
+		AcceuilPanel.add(lblNewLabel_3);
+		
+		JLabel lblSalleDattente = new JLabel("    Salle d'Attente");
+		lblSalleDattente.setIcon(new ImageIcon("ressources\\icons8-waiting-room-30.png"));
+		lblSalleDattente.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
+		lblSalleDattente.setBounds(22, 341, 185, 39);
+		AcceuilPanel.add(lblSalleDattente);
+		
+		JLabel lblConsultations = new JLabel("   Consultations");
+		lblConsultations.setIcon(new ImageIcon("ressources\\icons8-counselor-30.png"));
+		lblConsultations.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
+		lblConsultations.setBounds(22, 401, 185, 39);
+		AcceuilPanel.add(lblConsultations);
+		
+		JLabel canapeLabel = new JLabel("");
+		canapeLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				SalleAttente S=new SalleAttente();
+				S.setVisible(true);
+				setVisible(false);
+			}
+		});
+		
+		canapeLabel.setIcon(new ImageIcon("ressources\\canape.png"));
+		canapeLabel.setBounds(746, 341, 110, 99);
+		AcceuilPanel.add(canapeLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Salle d'attente");
+		lblNewLabel_2.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
+		lblNewLabel_2.setBounds(725, 325, 110, 20);
+		AcceuilPanel.add(lblNewLabel_2);
 		
 		JLabel outlookLabel = new JLabel("");
 		outlookLabel.setIcon(new ImageIcon("ressources\\outlook.png"));
 		outlookLabel.setBounds(10, 29, 63, 39);
-		panel_2.add(outlookLabel);
+		AcceuilPanel.add(outlookLabel);
 		
 		JLabel calendrierLabel = new JLabel("");
 		calendrierLabel.setIcon(new ImageIcon("ressources\\calendar.png"));
 		calendrierLabel.setBounds(10, 107, 63, 39);
-		panel_2.add(calendrierLabel);
+		AcceuilPanel.add(calendrierLabel);
 		
 		JLabel nextLabel = new JLabel("");
 		nextLabel.setIcon(new ImageIcon("ressources\\next.png"));
 		nextLabel.setBounds(793, 29, 63, 39);
-		panel_2.add(nextLabel);
+		AcceuilPanel.add(nextLabel);
 		
 		JLabel msgLabel = new JLabel("");
 		msgLabel.setIcon(new ImageIcon("ressources\\msg.png"));
 		msgLabel.setBounds(793, 107, 63, 39);
-		panel_2.add(msgLabel);
+		AcceuilPanel.add(msgLabel);
 		
 		JLabel label_3 = new JLabel("");
 
 		label_3.setBounds(0, -90, 866, 553);
-		panel_2.add(label_3);
+		AcceuilPanel.add(label_3);
 
 		label_3.setIcon(new ImageIcon("ressources\\backgroud ki ma tle3.png"));
 		label_3.setBackground(new Color(245, 255, 250));
@@ -149,7 +192,7 @@ public class Int_Medecin extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				patien.setVisible(true);
-				panel_2.setVisible(false);				
+				AcceuilPanel.setVisible(false);				
 				compte.setVisible(false);
 				
 				patien.setBounds(0, 90,866,846);
@@ -175,7 +218,7 @@ public class Int_Medecin extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				patien.setVisible(false);
-				panel_2.setVisible(false);				
+				AcceuilPanel.setVisible(false);				
 				compte.setVisible(true);
 				compte.setBounds(0, 90,866,846);
 				label_3.setVisible(false);
@@ -198,7 +241,7 @@ public class Int_Medecin extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				patien.setVisible(false);
-				panel_2.setVisible(true);				
+				AcceuilPanel.setVisible(true);				
 				compte.setVisible(false);
 			}
 		});
